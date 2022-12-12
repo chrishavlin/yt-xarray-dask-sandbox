7d427b75cc4b9c66441348dbc643519b4a44b0a4
Chris Havlin <chris.havlin@gmail.com>
2022-12-12 14:47:14 -0600

    get scripts working again

--- build_test_data.py
+++ build_test_data.py
@@ -1,4 +1,4 @@
-from daxryt import data
+from daxryt import data_handling
 import numpy as np
 from dask.distributed import Client
 import dask
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with dask.config.set({"distributed.scheduler.worker-saturation": "inf"}):
         c = Client(n_workers=6, threads_per_worker=1)
 
-        data.build_test_data(
+        data_handling.build_test_data(
             n_grids_xyz=(5, 5, 5),
             grid_wids_xyz=np.array([1., 1., 1.]),
             cells_per_grid_xyz=(50, 61, 54),
--- test_link.py
+++ test_link.py
@@ -27,34 +27,24 @@ if __name__ == "__main__":
     
     fields = list(ds_xr.data_vars)
     global_shape = ds_xr.data_vars[fields[0]].shape
-    global_left = np.array([ds_xr.coords[cn].min() for cn in ds_xr.dims.keys()])
-    global_right = np.array([ds_xr.coords[cn].max() for cn in ds_xr.dims.keys()])
-    bbox = np.column_stack([global_left, global_right])
-    
-    
-    # decompose the grid for using the xarray chunking 
-    chunk_info = chunk_inspector.get_chunk_info(ds_xr)
+
+    # decompose the grid for using the xarray chunking
+    c_w = chunk_inspector.ChunkWalker()
+    c_w.walk_the_chunks(ds_xr)
+    chunk_info = c_w.df
     chunk_info = chunk_info.rename(columns={'le': 'left_edge', 're': 'right_edge', 'size': 'dimensions'}) 
-    grid_data = chunk_info[['left_edge', 'right_edge', 'dimensions', 'si']].to_dict('records')
+    grid_data = chunk_info[['left_edge', 'right_edge', 'dimensions']].to_dict('records')
     starting_indices = []
-    n_grid_chunks = len(grid_data)  # (nprocs in load_uniform_grid)
+    n_grid_chunks = len(grid_data)
+    bbox = np.column_stack([c_w.global_le, c_w.global_re])
     
     # add on the reader to each grid
     for gid in range(n_grid_chunks):
         grid_data[gid]['level'] = 0
         for field in fields:
             grid_data[gid][field] = reader
-        starting_indices.append(grid_data[gid].pop('si'))
             
     ds = yt.load_amr_grids(grid_data, global_shape, bbox=bbox, geometry= ('cartesian', tuple(ds_xr.dims.keys())))
     ad = ds.all_data()
-    # this is probably erroring due to the yt get global start index
-    # start_index = (self.LeftEdge - self.ds.domain_left_edge) / self.dds
-    # self.start_index = np.rint(start_index).astype("int64").ravel()
-    # rounding error leading to si/ei that aren't quite right
-    # gs = ad[("stream", "gauss")]
-
-    for gid, g in enumerate(ds.index.grids):
-        g.start_index = starting_indices[gid]
     gs = ad[("stream", "gauss")]
     print(gs.min())
