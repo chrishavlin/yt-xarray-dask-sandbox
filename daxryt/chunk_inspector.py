a8a9a85ab63d326ba7b37a9a96f15bd2d0f1da80
Chris Havlin <chris.havlin@gmail.com>
2022-12-12 14:43:22 -0600

    clean up!

--- daxryt/chunk_inspector.py
+++ daxryt/chunk_inspector.py
@@ -32,18 +32,17 @@ def _get_single_coord_chunk_info(x, si: int, ei: int, nodes: bool = True):
 
 class ChunkWalker:
     def __init__(self):
-        # dont store the xr_dask_obj here... dont want to hold on to a
-        # reference
-        self.df: pd.DataFrame = None
-        self._initialize_walk()
 
-
-    def _initialize_walk(self):
+        # global state, calculated after recursion
         self.global_le = None
         self.global_re = None
         self.global_size = None
+        self.df: pd.DataFrame = None
 
-        # these all change during the recursion!
+        self._initialize_walk()  # initialize the recursion state variables
+
+    def _initialize_walk(self):
+        # chunk state during recursion: these all change during the recursion!
         self.chunk_info = []
         self.start_index = np.array([0, 0, 0])
         self.end_index = np.array([0, 0, 0])
@@ -54,10 +53,6 @@ class ChunkWalker:
         self.csize = np.array([0, 0, 0])  # chunk size
         self.c_dim_i = 0  # current dimension index
         self.ndims = 0 # total dimensions
-
-    def _cleanup_walk(self):
-        self.chunk_info = None
-
 
     def _find_global_stats(self):
         if self.df is None:
@@ -86,9 +81,7 @@ class ChunkWalker:
         self.ndims = len(coords_in_order)
         self._recursive_chonker(xr_dask_obj, dim_ordering, coords_in_order)
         self.df = pd.DataFrame(self.chunk_info).set_index('chunk_number')
-        self._cleanup_walk()
         self._find_global_stats()
-
 
     def _store_chunk(self):
         # stores the current state of the chunk info
@@ -106,6 +99,10 @@ class ChunkWalker:
                            xr_dask_obj,
                            dim_ordering,
                            coords_in_order):
+
+        # this recursively steps through the chunks of an xarray-dask object,
+        # recording the left/right edge of each chunk and more (see _store_chunk
+        # for all of the info stored on each chunk).
 
         for di_chunks in xr_dask_obj.chunks[dim_ordering[self.c_dim_i]]:
             self.end_index[self.c_dim_i] = self.start_index[self.c_dim_i] + di_chunks
@@ -125,75 +122,8 @@ class ChunkWalker:
             else:
                 # we must go deeper
                 self.c_dim_i += 1
-                self.start_index[self.c_dim_i] = 0
+                self.start_index[self.c_dim_i] = 0  # zero out
                 self._recursive_chonker(xr_dask_obj, dim_ordering, coords_in_order)
                 self.c_dim_i -= 1
 
             self.start_index[self.c_dim_i] += di_chunks
-
-#
-# def get_chunk_info(xr_dask_obj: Union[xr.Dataset, xr.DataArray], return_df: bool=True) -> Union[list, pd.DataFrame]:
-#
-#     # xr_dask_obj.chunks may be dict (for Dataset) or a tuple (for DataArray)
-#     # so need to regularize a bit:
-#     if isinstance(xr_dask_obj, xr.Dataset):
-#         dim_ordering = tuple(xr_dask_obj.dims.keys())  # e.g., x, y, z
-#         coords_in_order = dim_ordering  # e.g., x,y,z
-#     elif isinstance(xr_dask_obj, xr.DataArray):
-#         dim_ordering = (0, 1, 2)
-#         coords_in_order = xr_dask_obj.dims # e.g., x, y, z
-#     else:
-#         raise TypeError(f"type(xr_dask_obj)={type(xr_dask_obj)}, "
-#                         f"expected xr Dataset or DataArray")
-#
-#     chunk_info = []
-#     start_index = np.array([0, 0, 0])
-#     end_index = np.array([0, 0, 0])
-#     le = np.array([0., 0., 0.])  # left edge of each chunk
-#     re = np.array([0., 0., 0.])  # right edge of each chunk
-#
-#     ichunk = 0
-#     cwidth = np.array([0., 0., 0.]) # cell width
-#     csize = np.array([0, 0, 0])  # chunk size
-#     for d0_chunks in xr_dask_obj.chunks[dim_ordering[0]]:
-#         end_index[0] = start_index[0] + d0_chunks
-#
-#         le[0], re[0], cwidth[0] = _get_single_coord_chunk_info(xr_dask_obj.coords[coords_in_order[0]],
-#                                                                start_index[0],
-#                                                                end_index[0])
-#
-#         start_index[1] = 0
-#         for d1_chunks in xr_dask_obj.chunks[dim_ordering[1]]:
-#             end_index[1] = start_index[1] + d1_chunks
-#
-#             le[1], re[1], cwidth[1] = _get_single_coord_chunk_info(
-#                 xr_dask_obj.coords[coords_in_order[1]],
-#                 start_index[1],
-#                 end_index[1])
-#
-#             start_index[2] = 0
-#             for d2_chunks in xr_dask_obj.chunks[dim_ordering[2]]:
-#                 end_index[2] = start_index[2] + d2_chunks
-#
-#                 le[2], re[2], cwidth[2] = _get_single_coord_chunk_info(
-#                     xr_dask_obj.coords[coords_in_order[2]],
-#                     start_index[2],
-#                     end_index[2])
-#
-#                 chunk_info.append({'si': start_index.copy(),
-#                                    'ei': end_index.copy(),
-#                                    'size': np.array(
-#                                        [d0_chunks, d1_chunks, d2_chunks]),
-#                                    'le': le.copy(),
-#                                    're': re.copy(),
-#                                    'cell_widths': cwidth.copy(),
-#                                    'chunk_number': ichunk})
-#
-#                 start_index[2] += d2_chunks
-#                 ichunk += 1
-#             start_index[1] += d1_chunks
-#         start_index[0] += d0_chunks
-#
-#     if return_df:
-#         return pd.DataFrame(chunk_info).set_index('chunk_number')
-#     return chunk_info
